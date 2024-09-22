1ab256cf22fc37e9aa5a2591a587b9a5d96d2900
Dima1385 <Lozik1385@gmail.com>
2024-09-22 13:13:32 +0300

    add CRUD

---
+++ Dashboard.DAL/Repositories/RoleRepository/RoleRepository.cs
@@ -0,0 +1,70 @@
+﻿using Dashboard.DAL.Data;
+using Dashboard.DAL.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Dashboard.DAL.Repositories.RoleRepository
+{
+    public interface IRoleRepository
+    {
+        Task<Role> GetByIdAsync(Guid id);
+        Task<Role> GetByNameAsync(string name);
+        Task<IEnumerable<Role>> GetAllAsync();
+        Task AddAsync(Role role);
+        Task UpdateAsync(Role role);
+        Task DeleteAsync(Guid id);
+    }
+
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RoleRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Role> GetByIdAsync(Guid id)
+        {
+            return await _context.Roles.FindAsync(id);
+        }
+
+        public async Task<Role> GetByNameAsync(string name)
+        {
+            return await _context.Roles.FirstOrDefaultAsync(r => r.Name == name);
+        }
+
+        public async Task<IEnumerable<Role>> GetAllAsync()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        public async Task AddAsync(Role role)
+        {
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Role role)
+        {
+            _context.Roles.Update(role);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var role = await GetByIdAsync(id);
+            if (role != null)
+            {
+                _context.Roles.Remove(role);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
